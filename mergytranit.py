74a45c74deb114a0e1ff6ab6134af5a24cd27504
chelseah <huangxpku@gmail.com>
2013-02-26 11:23:08 -0500

    add in a wrapper

---
+++ mergytranit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import scipy as sp
+import math
+"""
+This is a wrapper for sorttransit so that it 
+run on a list of file
+"""
+def fileexists(ID,ap,pmax):
+    blsanalf = ID+'.blsanal' 
+    reconblsanal = ID+'.AP%d' % ap + 'P%d' % (pmax-1)+ '.blsanal' 
+    if(os.path.exists(blsanalf) and os.path.exists(reconblsanal)):
+        return True
+    else:
+        return False
+def main():
+    colp = 2
+    coldsp = 14
+    maxp = 5
+    inpath = '/home/chelsea/KEP/G365/TFA/'
+    os.chdir(inpath)
+    for ap in xrange(3):
+        inlist = '/home/chelsea/KEP/G365/bin/AP%d.ls' % ap
+        names = []; readcolumn(names,1,inlist,dataformat='str')
+        for x in names:
+            if(fileexists(ID,ap,pmax)):
+                outfile = x+'.mergy.blsanal'
+                os.system("sorttransit -i %s -o %s -p %d -d %d -a %d -m %d --noharm" % x, outfile, colp,coldsp,ap,maxp)
+    return
+if __name__=='__main__':
+    main()
